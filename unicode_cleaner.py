56b57da0c54d6e7447abeeda01df1007c08118c9
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2022-07-10 21:04:37 -0400

    add unicode normalizer

---
+++ unicode_cleaner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys, argparse, unicodedata
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_path", type=Path)
+
+p = parser.parse_args()
+
+if p.file_path.exists():
+  with open(p.file_path, "r+") as file:
+    lines = file.readlines()
+
+    for line in lines:
+      normalized = unicodedata.normalize('NFKC', line).encode('utf8', errors='ignore').decode()
+      print(f"Line: {line}")
+      print(f"Normalization: {normalized}")
+      line = normalized
+    
+    file.truncate(0)
+    file.writelines(lines)
+    file.close()
+    sys.exit("File Normalized")
+else:
+  sys.exit("File Not Found")
